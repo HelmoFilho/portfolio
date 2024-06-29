2eb928cec7604116544478211eed80d28cb7411c
HelmoFilho <helmofilho09@gmail.com>
2024-06-29 09:46:26 -0300

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/[locale]/globals.css";
 
 import MyThemeProvider from "@/providers/MyThemeProvider";
 
